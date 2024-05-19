8de47520c44e1c2d1eee14619641f9be1d67525e
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-19 22:27:33 +0300

    New commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""simple app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """say hello"""
+    return "Hello HBNB!"
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """say hbnb"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
