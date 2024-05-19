c1881fb93569b915d75cc1107af1b7fa3a05a5ce
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-19 15:58:58 +0300

    New commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """simple app"""
 from flask import Flask
 
@@ -9,4 +10,5 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def Hello():
+    """say hello"""
     return "Hello HBNB!"
