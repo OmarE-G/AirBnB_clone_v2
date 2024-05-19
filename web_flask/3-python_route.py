9d8fa234a73cb3c5fe36f51bfe65e44ba286ff8a
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-19 22:45:57 +0300

    New commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""simple app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """say hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """say hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """say c is text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pytext(text="is cool"):
+    """say c is text"""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
