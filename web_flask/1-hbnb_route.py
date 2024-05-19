f325b136da05b2b781d92801238cd8db8727c47a
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-19 22:28:13 +0300

    New commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def Hello():
     """say hello"""
     return "Hello HBNB!"
 
+
 @app.route('/', strict_slashes=False)
 def Hello():
     """say hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
