7ba4576e3396d1ca990d3cd34f6da7f7c4ce8ef5
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-19 22:42:37 +0300

    New commit

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def ctext(text):
     """say c is text"""
-    return "C is " + text.replace('_', ' ')
+    return "C " + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
